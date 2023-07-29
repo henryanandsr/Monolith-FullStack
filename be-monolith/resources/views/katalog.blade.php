56d3abfffb936bd9a38e3644b3f61b605e1beb76
henryanandsr <13521004@std.stei.itb.ac.id>
2023-07-29 20:46:53 +0700

    revert

--- be-monolith/resources/views/katalog.blade.php
+++ be-monolith/resources/views/katalog.blade.php
@@ -40,11 +40,42 @@
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
-        function refreshData() {
-            location.reload();            
+        var currentPage = 1;
+
+        function paginate(pageNumber) {
+            currentPage = pageNumber;
+            refreshData();
         }
+
+        function refreshData() {
+            $.get('/katalog-barang?page=' + currentPage, function(data) {
+                $('#barangs').empty();
+                $('#pagination').empty();
+
+                $.each(data.barangs, function(i, barang) {
+                    $('#barangs').append(`
+                        <div class="p-4 m-4 bg-blue-50 rounded-lg shadow-md">
+                            <div class="flex justify-between">
+                                <div>
+                                    <h2 class="text-gray-700 font-bold text-xl">${barang.nama}</h2>
+                                    <p>Harga : Rp ${barang.harga}</p>
+                                    <p>Stock : ${barang.stok}</p>
+                                </div>
+                                <div>
+                                    <a href="/barang/${barang.id}" class="font-bold text-blue-500">Detail</a>
+                                </div>
+                            </div>
+                        </div>
+                    `);
+                });
+
+                for (var i = 1; i <= data.pages; i++) {
+                    $('#pagination').append(`<button onclick="paginate(${i})" class="bg-blue-500 text-white px-4 py-2 rounded-md">${i}</button>`);
+                }
+            });
+        }
+
         setInterval(refreshData, 1000);
     </script>
 </body>
